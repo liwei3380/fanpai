363c93f7d699592269db2e9ec5944684f8f21985
liwei <liwei@qq.com>
2017-06-09 15:01:28 +0800

    完善动画修过，添加结束页面

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -7,10 +7,12 @@
     </div>
     <div class="card-group-wrap clearfix">
       <div class="card-wrap" v-for="item in items" >
-        <transition name="cardhide">
+        <transition name="bounce">
           <div :class="item.className2" v-show="item.show">{{ item.message }}</div>
         </transition>
+        <transition name="bounce2">
           <div :class="item.className" v-show="item.show" @click="clickcard(item,$event)"></div>
+        </transition>
         
       </div>
     </div>
@@ -24,28 +26,33 @@ export default {
     return {
       msg: 'Welcome to Your Vue.js App',
       originItems: [
-        {message:0,className:'card',className2:'card2',show:true},
-        {message:1,className:'card',className2:'card2',show:true},
-        {message:2,className:'card',className2:'card2',show:true},
-        {message:3,className:'card',className2:'card2',show:true},
-        {message:4,className:'card',className2:'card2',show:true},
-        {message:5,className:'card',className2:'card2',show:true},
-        {message:6,className:'card',className2:'card2',show:true},
-        {message:7,className:'card',className2:'card2',show:true},
-        {message:8,className:'card',className2:'card2',show:true},
-        {message:9,className:'card',className2:'card2',show:true},
-        {message:10,className:'card',className2:'card2',show:true},
-        {message:11,className:'card',className2:'card2',show:true}
+        {message:0,className:'card',className2:'card2',show:false},
+        {message:1,className:'card',className2:'card2',show:false},
+        {message:2,className:'card',className2:'card2',show:false},
+        {message:3,className:'card',className2:'card2',show:false},
+        {message:4,className:'card',className2:'card2',show:false},
+        {message:5,className:'card',className2:'card2',show:false},
+        {message:6,className:'card',className2:'card2',show:false},
+        {message:7,className:'card',className2:'card2',show:false},
+        {message:8,className:'card',className2:'card2',show:false},
+        {message:9,className:'card',className2:'card2',show:false},
+        {message:10,className:'card',className2:'card2',show:false},
+        {message:11,className:'card',className2:'card2',show:false}
         ],
       items: [],
       time: 59,
       score: 0,
       checkedResult: [],
       leftCount: 12,
+      gameover: false
     }
   },
   methods: {
     clickcard(item){
+
+      if (this.gameover) {
+        return
+      }
 
       if (item.className == 'card') {
         item.className = 'card checked'
@@ -59,18 +66,25 @@ export default {
         let result = parseInt(this.checkedResult[0].message) + parseInt(this.checkedResult[1].message)
         // 匹配成功
         if (result == 11) {
+          let vm = this
+          setTimeout(function(){
+            vm.checkedResult[0].show = false
+            vm.checkedResult[1].show = false
+
+            // 数组移除隐藏的卡
+            for(let i = 0; i < 2; i ++){
+              vm.checkedResult.shift()
+            }
+            vm.leftCount -= 2
+            vm.score += 1
+            if (vm.leftCount == 0) {
+              vm.initItems(1)
+            }
+          },150)
           // 2个匹配的卡隐藏
-          this.checkedResult[0].show = false
-          this.checkedResult[1].show = false
-          // 数组移除隐藏的卡
-          for(let i = 0; i < 2; i ++){
-            this.checkedResult.shift()
-          }
-          this.leftCount -= 2
-          this.score += 1
-          if (this.leftCount == 0) {
-            this.initItems()
-          }
+          /*this.checkedResult[0].show = false
+          this.checkedResult[1].show = false*/
+          
         }
       } else if(this.checkedResult.length == 3) {
         // 选择了3个卡
@@ -82,14 +96,20 @@ export default {
         }
       }
     },
-    initItems () {
-      this.items = this.originItems
-      this.items.shuffle()
-      for (var i = 0; i < this.items.length; i++) {
-        this.items[i].show = true
-        this.items[i].className = 'card'
-        this.items[i].className2 = 'card2'
-      }
+    initItems (p) {
+      
+      let vm = this
+      vm.items = vm.originItems
+      vm.items.shuffle()
+      vm.leftCount = 12
+      vm.checkedResult = []
+      setTimeout(function(){
+        for (var i = 0; i < vm.items.length; i++) {
+          vm.items[i].show = true
+          vm.items[i].className = 'card'
+          vm.items[i].className2 = 'card2'
+        }
+      },250)
     }
   },
   mounted () {
@@ -97,20 +117,21 @@ export default {
       Array.prototype.shuffle = function() {
           for(var j, x, i = this.length; i; j = parseInt(Math.random() * i), x = this[--i], this[i] = this[j], this[j] = x);
           return this;
-    };
+      }
+    }
     let vm = this
+    console.log(vm)
     vm.initItems()
     vm.si = setInterval(function(){
-          if(vm.time > 1){
+          if(vm.time >= 1){
             vm.time -= 1
           } else {
             clearInterval(vm.si)
-            vm.gaming = true
+            vm.gameover = true
+            sessionStorage.setItem('score',vm.score)
+            vm.$router.push('/over')
           }
         },1000)
-
-    
-}
   }
 }
 </script>
@@ -138,20 +159,24 @@ export default {
   width:1.8rem;
   height: 2rem;
   margin: .1rem;
-  background: #FB5B39;
+  /*background: #FB5B39;*/
+  background: url('../assets/cardback.png');
+  background-size: cover;
   position: absolute;
   transform-style: preserve-3d;
   backface-visibility: hidden;
-  transition:0.5s ease-in-out ;
+  transition:0.12s ease-in-out ;
 }
 .card2{
   width:1.8rem;
   height: 2rem;
   margin: .1rem;
-  background: #FB5B39;
+  /*background: #46FA3B;*/
+  background: url('../assets/card1.png');
+  background-size: cover;
   position: absolute;
   transform-style: preserve-3d;
-  transition:0.5s ease-in-out;
+  transition:0.12s ease-in-out;
   transform: rotateY(-180deg);
 }
 .checked{
@@ -174,9 +199,39 @@ export default {
   overflow: hidden;
 }
 .cardhide-leave-active {
-  transition: transform 1s
+  transition: opacity 1s
 }
 .cardhide-leave-to /* .fade-leave-active in <2.1.8 */ {
-  transform: scale(0);
+  opacity: 0.1;
+}
+.bounce2-enter-active {
+  animation: bounce-in .5s;
+}
+.bounce-leave-active {
+  animation: bounce-out .5s;
+}
+.bounce-enter-active {
+  animation: bounce-in .5s;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+    opacity: 1;
+  }
+  100% {
+    transform: scale(1);
+    opacity: 1;
+  }
+}
+@keyframes bounce-out {
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.2);
+  }
+  100% {
+    transform: scale(0);
+  }
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Game from '@/components/Game'
+import Over from '@/components/Over'
 
 Vue.use(Router)
 
@@ -15,6 +16,10 @@ export default new Router({
       path: '/game',
       name: 'Game',
       component: Game
+    },{
+      path: '/over',
+      name: 'Over',
+      component: Over
     }
   ]
 })
